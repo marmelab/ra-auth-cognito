30092ec0411afc551dd8f65786fe623bb4c66f01
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-01-05 17:23:54 +0100

    Add OAuth support

--- packages/ra-auth-cognito/src/authProvider.ts
+++ packages/ra-auth-cognito/src/authProvider.ts
@@ -1,6 +1,9 @@
-import { AuthProvider } from 'react-admin';
+import { AuthProvider, HttpError } from 'react-admin';
 import {
     AuthenticationDetails,
+    CognitoAccessToken,
+    CognitoIdToken,
+    CognitoRefreshToken,
     CognitoUser,
     CognitoUserPool,
     CognitoUserSession,
@@ -44,10 +47,35 @@ import { ErrorRequireNewPassword } from './ErrorRequireNewPassword';
  * @param userPool a CognitoUserPool instance
  * @returns an authProvider ready to be used by React-Admin.
  */
+// https://react-admin.auth.eu-west-3.amazoncognito.com/login?client_id=1v090rrs9pi78u5cci11o9u6bp&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth-callback
+export type CognitoAuthProviderOptionsPool = CognitoUserPool;
+
+export type CognitoAuthProviderOptionsIds = {
+    userPoolId: string;
+    clientId: string;
+    mode: 'oauth' | 'username';
+    redirect_uri?: string;
+    scope?: string[];
+};
+
+export type CognitoAuthProviderOptions =
+    | CognitoAuthProviderOptionsPool
+    | CognitoAuthProviderOptionsIds;
+
 export const CognitoAuthProvider = (
-    userPool: CognitoUserPool
+    options: CognitoAuthProviderOptions
 ): AuthProvider => {
     let user: CognitoUser | null = null;
+    const mode = options instanceof CognitoUserPool ? 'username' : options.mode;
+
+    const userPool =
+        options instanceof CognitoUserPool
+            ? (options as CognitoUserPool)
+            : new CognitoUserPool({
+                  UserPoolId: options.userPoolId,
+                  ClientId: options.clientId,
+              });
+
     return {
         async login({
             username,
@@ -119,11 +147,34 @@ export const CognitoAuthProvider = (
         },
         // called when the user navigates to a new location, to check for authentication
         async checkAuth() {
-            return new Promise((resolve, reject) => {
-                const user = userPool.getCurrentUser();
+            return new Promise<void>((resolve, reject) => {
+                const redirectToOAuthIfNeeded = (error?: Error) => {
+                    if (mode === 'oauth') {
+                        const oauthOptions = options as CognitoAuthProviderOptionsIds;
+                        const redirect_uri =
+                            oauthOptions.redirect_uri ??
+                            `${window.location.origin}/auth-callback`;
+                        const scope = oauthOptions.scope || [
+                            'openid',
+                            'email',
+                            'profile',
+                        ];
+                        const url = `https://react-admin.auth.eu-west-3.amazoncognito.com/login?client_id=${
+                            oauthOptions.clientId
+                        }&response_type=token&scope=${scope.join(
+                            '+'
+                        )}&redirect_uri=${redirect_uri}`;
+                        window.location.href = url;
+                    } else {
+                        return reject(new HttpError('No user', 401));
+                    }
+                };
+                let user = userPool.getCurrentUser();
 
                 if (!user) {
-                    return reject();
+                    return redirectToOAuthIfNeeded(
+                        new HttpError('No user', 401)
+                    );
                 }
                 user.getSession((err, session) => {
                     if (err) {
@@ -131,7 +182,9 @@ export const CognitoAuthProvider = (
                     }
 
                     if (!session.isValid()) {
-                        return reject();
+                        return redirectToOAuthIfNeeded(
+                            new HttpError('No user', 401)
+                        );
                     }
 
                     user.getUserAttributes(err => {
@@ -195,6 +248,41 @@ export const CognitoAuthProvider = (
                     });
                 });
             });
+        },
+        async handleCallback() {
+            const urlParams = new URLSearchParams(
+                window.location.hash.substr(1)
+            );
+            const error = urlParams.get('error');
+            const errorDescription = urlParams.get('error_description');
+            const idToken = urlParams.get('id_token');
+            const accessToken = urlParams.get('access_token');
+
+            if (error) {
+                throw new Error(errorDescription);
+            }
+
+            if (idToken == null || accessToken == null) {
+                throw new Error('Failed to handle login callback.');
+            }
+            const session = new CognitoUserSession({
+                IdToken: new CognitoIdToken({ IdToken: idToken }),
+                RefreshToken: new CognitoRefreshToken({
+                    RefreshToken: null,
+                }),
+                AccessToken: new CognitoAccessToken({
+                    AccessToken: accessToken,
+                }),
+            });
+            const user = new CognitoUser({
+                Username: session.getIdToken().decodePayload()[
+                    'cognito:username'
+                ],
+                Pool: userPool,
+                Storage: window.localStorage,
+            });
+            user.setSignInUserSession(session);
+            return user;
         },
     };
 };
